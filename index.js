4b5bb7c7d4bc2477d970d28d9ccb61ae27b0199c
Mathew Sida <Sida.mathew@gmail.com>
2022-03-16 04:28:04 +0000

    broken

--- index.js
+++ index.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-app.set('view enine', 'ejs');
+app.set('view enine','ejs');
+
+
 
 app.get('/', (req, res) => {
   var title = 'Our Home Page';
 	res.render('index', {title:title});
 });
+
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
