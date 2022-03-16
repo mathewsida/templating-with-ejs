114a8b82ebdcfa4cdb652614ce31072d93ebd620
Mathew Sida <Sida.mathew@gmail.com>
2022-03-16 03:52:58 +0000

    title

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view enine', 'ejs');
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
+  var title = 'Our Home Page';
+	res.render('index', {title:title});
+});
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
