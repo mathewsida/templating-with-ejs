914b46f674004cef3701de647b4538ff81c1aaee
Mathew Sida <Sida.mathew@gmail.com>
2022-03-18 01:08:03 +0000

    list thing

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const app = express();
 const port = 3000
 
+
+var data = require('./data/test.json');
 app.set('view engine', 'ejs');
 
 
@@ -9,16 +11,44 @@ app.set('view engine', 'ejs');
 app.use(express.static(__dirname));
 
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
   var title = 'Our Home Page'
   res.render('pages/index', {title:title});
 });
+
+app.get('/home', (req, res) => {
+  var title = 'Our Home Page'
+  res.render('pages/home', {title:title});
+});
+
 
 //about us
 app.get('/about-us', (req, res) => {
   var title = 'Our About Us Page'
   res.render('pages/about-us', {title:title});
 });
+
+
+//add users route
+app.get('/users', function(req, res) {
+	var title = 'Users Page';
+	res.render('users/index', {
+    	title: title,
+    	users: data
+	});
+});
+
+//add user/view route - we are cheating by using the array index - 1
+app.get('/users/view/:id', function(req, res) {
+ var title = 'User Page';
+ var id = req.params.id;
+ res.render('users/view', {
+     title: title,
+     user: data[--id]
+ });
+});
+
+
 
 		app.get('/halo1', (req, res) => {
 		  var title = 'Halo Combat Evolved'
@@ -44,4 +74,5 @@ app.get('/about-us', (req, res) => {
 
 app.listen(port, () => {
   console.log('Example app listening on port ${port}');
+	console.log(data);
 });
